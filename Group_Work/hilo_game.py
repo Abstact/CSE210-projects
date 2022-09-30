6ca6f89ae43ab5ab120fdb50356cc8743d155552
CJLynch01 <cjlynch@byui.edu>
2022-09-29 19:02:21 -0600

    Inputted test comment

--- Group_Work/hilo_game.py
+++ Group_Work/hilo_game.py
@@ -2,7 +2,7 @@ import random
 
 def main():
     pass
-
+# This is Chris's Comment
 class Director:
     #THIS IS A COMMENT 
     def __init__(self):
@@ -11,11 +11,9 @@ class Director:
 class Card:
     def __init__(self):
         pass
-   
+    # test commet
     def getCard():
         return random.randrange(1,13)
 
 if __name__ == "__main__":
     main()
-
-# just a comment
