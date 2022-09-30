21e07b55501909a92bd72a0cac7918af96c10925
narowley <nathanarowley@gmail.com>
2022-09-29 18:40:20 -0600

    HiLo Game

---
+++ Group_Work/hilo_game.py
@@ -0,0 +1,15 @@
+import random
+
+def main():
+    pass
+
+class Director: 
+    def __init__(self):
+        pass
+
+class Card:
+    def __init__(self):
+        pass
+
+if __name__ == "__main__":
+    main()
