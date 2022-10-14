897e5a76bbdcbd25156f6b8264d45be0122ee747
Abstact <jogden1000@gmail.com>
2022-10-13 20:50:08 -0400

    Added base of terminal service class

---
+++ Group_Work/jumper_game/game/terminal_service.py
@@ -0,0 +1,13 @@
+"""
+Terminal Service
+Variables: None
+Functions:
+    Player input
+        - makes sure that user inputs is contained within [a-z]
+    Rules prompt
+        - prints out rules for game
+
+"""
+class Terminal_service:
+    def Rules_prompt():
+        print("Rules contained here")
