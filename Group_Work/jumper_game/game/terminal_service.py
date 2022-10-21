d4926b88f8f9a886bab73ca43abaec84d3bda94f
Abstact <jogden1000@gmail.com>
2022-10-21 12:11:39 -0400

    Added terminal method stubs, to complete later today

--- Group_Work/jumper_game/game/terminal_service.py
+++ Group_Work/jumper_game/game/terminal_service.py
@@ -2,12 +2,32 @@
 Terminal Service
 Variables: None
 Functions:
-    Player input
-        - makes sure that user inputs is contained within [a-z]
     Rules prompt
         - prints out rules for game
+    print_word(string)
+        -
+    print_guy(int)
+        -
+    game_over()
+        -
+    game_won(string)
+        -
 
 """
 class Terminal_service:
     def Rules_prompt():
-        print("Rules contained here")
+        print("Welcome to Jumper!")
+        print("You are tasked with guessing a word one letter at a time. Be careful though, as each wrong letter will take some of your parachute away! Too many wrong guesses will cause you to make a quicker landing than was planned.")
+
+    
+    def print_word(guesses):
+        pass
+
+    def print_guy(hp):
+        pass
+
+    def game_over():
+        pass
+
+    def game_won(word):
+        pass
