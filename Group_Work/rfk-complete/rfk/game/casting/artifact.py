82bf6501d77f75ccc55ea738509dddacf7a75834
CJLynch01 <cjlynch@byui.edu>
2022-11-04 23:31:04 -0600

    update to artifact

--- Group_Work/rfk-complete/rfk/game/casting/artifact.py
+++ Group_Work/rfk-complete/rfk/game/casting/artifact.py
@@ -1,5 +1,7 @@
-from game.casting.actor import Actor
-
+import random
+from game.casting.actor import Actor;
+from game.shared.color import Color;
+from game.shared.point import Point;
 
 class Artifact(Actor):
     """
@@ -16,3 +18,10 @@ class Artifact(Actor):
 
         super().set_points(+1) #this adds a point for each artifact hit
         
+        speed = random.randrange(1,15) # set the velocity
+        super().set_velocity(Point(0,speed))
+
+        red = random.randrange(150,200)
+        green = random.randrange(50, 75) 
+        color = Color(red, green, green) # random dark red color
+        super().set_color(color)
