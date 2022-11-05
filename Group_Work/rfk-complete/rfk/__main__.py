3209ed7c14e915f6bab0a16a4473dc45764b72c5
CJLynch01 <cjlynch@byui.edu>
2022-11-05 00:02:20 -0600

    removed unnecessary artifacts

--- Group_Work/rfk-complete/rfk/__main__.py
+++ Group_Work/rfk-complete/rfk/__main__.py
@@ -54,31 +54,31 @@ def main():
     cast.add_actor("robots", robot)
     
     # create the artifacts
-    with open(DATA_PATH) as file:
-        data = file.read()
-        messages = data.splitlines()
+    # with open(DATA_PATH) as file:
+    #     data = file.read()
+    #     messages = data.splitlines()
 
-    for n in range(DEFAULT_ARTIFACTS):
-        text = chr(random.randint(33, 126))
-        message = messages[n]
+    # for n in range(DEFAULT_ARTIFACTS):
+    #     text = chr(random.randint(33, 126))
+    #     message = messages[n]
 
-        x = random.randint(1, COLS - 1)
-        y = random.randint(1, ROWS - 1)
-        position = Point(x, y)
-        position = position.scale(CELL_SIZE)
+    #     x = random.randint(1, COLS - 1)
+    #     y = random.randint(1, ROWS - 1)
+    #     position = Point(x, y)
+    #     position = position.scale(CELL_SIZE)
 
-        r = random.randint(0, 255)
-        g = random.randint(0, 255)
-        b = random.randint(0, 255)
-        color = Color(r, g, b)
+    #     r = random.randint(0, 255)
+    #     g = random.randint(0, 255)
+    #     b = random.randint(0, 255)
+    #     color = Color(r, g, b)
         
-        artifact = Artifact()
-        artifact.set_text(text)
-        artifact.set_font_size(FONT_SIZE)
-        artifact.set_color(color)
-        artifact.set_position(position)
-        # artifact.set_message(message)
-        cast.add_actor("artifacts", artifact)
+    #     artifact = Artifact()
+    #     artifact.set_text(text)
+    #     artifact.set_font_size(FONT_SIZE)
+    #     artifact.set_color(color)
+    #     artifact.set_position(position)
+    #     # artifact.set_message(message)
+    #     cast.add_actor("artifacts", artifact)
 
     # artifact = artifact + SPEED
     # if artifact > MAX_Y:
