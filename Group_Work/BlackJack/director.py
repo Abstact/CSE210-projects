af0de1e4cb9b4a1c8702e622afe9d5e025d0df2b
narowley <nathanarowley@gmail.com>
2022-12-05 17:12:04 -0700

    Added score mechanic

--- Group_Work/BlackJack/director.py
+++ Group_Work/BlackJack/director.py
@@ -10,6 +10,8 @@ class Director():
     def __init__(self):
         self.playing = True #self.playing - Determines whether or not the game is being played or if the player has ended the game
         self.cards_dealt = False #self.cards_dealt - Prevents the while loop for dealing cards twice
+        self.player_win_count = 0
+        self.dealer_win_count = 0
         self.player = Player() #Remaining variables are just initializing classes
         self.dealer = Dealer()
         self.terminal = Terminal()
@@ -50,17 +52,21 @@ class Director():
     
     def results(self):
         """
-            Logic on who wins the game. Uses terminal to display appropriate message
+            Logic on who wins the game and uses terminal to display appropriate message. Displays score afterwards
         """
         if not self.playing:
             return
 
         if self.player.card_total > self.dealer.card_total:
             self.terminal.player_win()
+            self.player_win_count += 1
         elif self.player.card_total < self.dealer.card_total:
             self.terminal.dealer_win()
+            self.dealer_win_count += 1
         elif self.player.card_total == self.dealer.card_total:
             self.terminal.tie()
+        
+        self.terminal.score(self.player_win_count, self.dealer_win_count)
 
     def reset(self):
         """
